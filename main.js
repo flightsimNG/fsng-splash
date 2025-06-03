31fecd8687d405239ffc6b80449127e5d207491b
polylogue <evanodoch@gmail.com>
2025-06-03 22:25:06 +0100

    Added User Agent + testing enhancements

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const os = require('os');
+
+const platform = os.type();
+const release = os.release();
+const electronVersion = process.versions.electron;
+const appVersion = app.getVersion();
+
+const userAgent = `fsNG/${appVersion} (${platform} ${release}; Electron ${electronVersion})`;
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -12,7 +20,8 @@ function createWindow() {
         frame: false,
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false
+            contextIsolation: false,
+            userAgent: userAgent
         }
     });
 
@@ -21,6 +30,11 @@ function createWindow() {
 
     win.webContents.on('devtools-opened', () => {
         win.webContents.closeDevTools();
+        win.webContents.session.webRequest.onBeforeSendHeaders((details, callback) => {
+        details.requestHeaders['User-Agent'] = userAgent;
+        callback({ cancel: false, requestHeaders: details.requestHeaders });
+        });
+
     });
     win.webContents.on('before-input-event', (event, input) => {
         if ((input.control || input.meta) && input.key.toLowerCase() === 'i' && input.type === 'keyDown') {
